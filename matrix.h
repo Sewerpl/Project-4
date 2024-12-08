8f914c1883a5e56d37d4d124638b4e7069255401
PC <seweryn.contact@gmail.com>
2024-12-08 21:22:04 +0100

    klasa public, konstruktor destruktor i metody zarządzania pamięcią

--- matrix.h
+++ matrix.h
@@ -14,6 +14,24 @@ private:
     void freeMemory(); // metoda pomocnicza do zwolnienia pamięci
     void allocateMemory(int n); // metoda pomocnicza do alokacji pamięci
 
+public:
+    // Konstruktor i destruktor
+    matrix();
+    matrix(int n);
+    matrix(int n, int* t);
+    matrix(const matrix& m); // konstruktor kopiujący
+    ~matrix();
+
+    // Metody zarządzania pamięcią
+    matrix& alokuj(int n);
+
+    // Metody dostępu i modyfikacji elementów
+    matrix& wstaw(int x, int y, int wartosc);
+    int pokaz(int x, int y) const;
+
+
+
+
 };
 
 #endif // MATRIX_H
